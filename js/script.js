aca77df208421b8f74b60fdae66a79a193b85916
jmn2069 <30607843+jmn2069@users.noreply.github.com>
2022-10-18 19:10:13 -0700

    add clear & color picker

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const container = document.getElementById('container');
-const btn = document.getElementById('changeGrid');
+const btnGrid = document.getElementById('changeGrid');
+const btnClear = document.getElementById('clear');
+let userColor = document.getElementById('userColor');
+let manualColor = false;
+
+userColor.addEventListener('click', function() { setUserColor(); });
+
+function setUserColor() {
+    manualColor = true;
+    gridSquares.forEach(grid => grid.style.opacity = '1');
+}
+
+// const gridSquares = document.querySelectorAll('.grid');
 let gridSize = 16;
 
-btn.addEventListener('click', function() { changeGrid(); });
+btnGrid.addEventListener('click', function() { changeGrid(); });
+btnClear.addEventListener('click', function() { clear(); });
+
+function clear() {
+    const gridSquares = document.querySelectorAll('.grid');
+    gridSquares.forEach(grid => grid.style.removeProperty('background-color'));
+    manualColor = false;
+}
 
 function changeGrid() {
     gridSize = prompt("Enter a number from 1-100")
@@ -35,15 +54,33 @@ function buildGrid() {
     gridSquares.forEach(grid => grid.style.width = (960 / gridSize) + "px");
     gridSquares.forEach(grid => grid.style.height = (960 / gridSize) + "px");
 }
+var mouseIsDown = false;
+
+window.addEventListener('mousedown', function() { mouseIsDown = true; });
+
+window.addEventListener('mouseup', function() { mouseIsDown = false; });
 
 function colorPicker() {
-    if (!this.style.backgroundColor || this.style.opacity < .1){
-        let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        this.style.backgroundColor = '#' + randomColor;
+    if (mouseIsDown && manualColor) {
+        this.style.backgroundColor = userColor.value;
         this.style.opacity = 1;
-    } else {
-        this.style.opacity -= .1;
+        return;
     }
-}
+        if (mouseIsDown && (!this.style.backgroundColor || this.style.opacity < .1)){
+            let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            this.style.backgroundColor = '#' + randomColor;
+            this.style.opacity = 1;
+        } else if (mouseIsDown) {
+            this.style.opacity -= .1;
+        }
+    }
+
+    window.addEventListener('dragstart', (e) => {
+        e.preventDefault()
+      })
+      
+      window.addEventListener('drop', (e) => {
+        e.preventDefault()
+      })
 
 buildGrid();
